3cfb77322aed5d451d9986a28005bf055b8e0a1e
Lucas Lopes da Silva <okasanjelucas@gmail.com>
2024-02-25 12:00:43 -0300

    Add: Src folder and main file

---
+++ src/__init__.py
@@ -0,0 +1,12 @@
+'''
+    EN:
+    This module exists just to initialize the src folder and, with this, facilitate the
+    imports to the modules out of this last (src).
+
+    PT:
+    Esse módulo existe apenas para inicializar a pasta src e, com isso, facilitar as
+    importações para os módulos que estão fora desse (src).
+'''
+
+from src import imports
+from src import defs
